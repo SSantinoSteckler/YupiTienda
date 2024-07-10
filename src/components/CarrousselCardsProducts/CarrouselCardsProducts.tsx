dae1dff55401529de2c32f53e160a59d45159e0c
Lucas Santino Steckler <lucassteckler1@gmail.com>
2024-07-10 01:58:35 -0300

    v.4

--- src/components/CarrousselCardsProducts/CarrouselCardsProducts.tsx
+++ src/components/CarrousselCardsProducts/CarrouselCardsProducts.tsx
@@ -14,40 +14,17 @@ export const CarouselProduct = ({ products }: CarouselProductProps) => {
 
   const halfLength = Math.ceil(products.length / 2);
 
-  const firstHalf = products.slice(0, halfLength);
-  const secondHalf = products.slice(halfLength);
-
-  const remainingProducts = settings.slidesToShow - secondHalf.length;
-  const productsForSecondCarousel = firstHalf.slice(remainingProducts);
-
   return (
     <div className='w-full max-w-[1500px] mx-auto my-8'>
       <div className='my-10'>
         <Slider {...settings}>
-          {firstHalf.map((product) => (
+          {products.slice(0, halfLength).map((product) => (
             <div key={product.id} className='p-2'>
               <CardProduct elem={product} />
             </div>
           ))}
         </Slider>
       </div>
-      {secondHalf.length > 0 && (
-        <div className='my-10'>
-          <Slider {...settings}>
-            {secondHalf.map((product) => (
-              <div key={product.id} className='p-2'>
-                <CardProduct elem={product} />
-              </div>
-            ))}
-
-            {productsForSecondCarousel.map((product) => (
-              <div key={product.id} className='p-2'>
-                <CardProduct elem={product} />
-              </div>
-            ))}
-          </Slider>
-        </div>
-      )}
     </div>
   );
 };
